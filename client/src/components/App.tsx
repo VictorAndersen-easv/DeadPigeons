69f6b18c635ce74b67103b5d0c365780b3adfe40
Victor Andersen <vicand01@easv365.dk>
2025-12-09 11:19:30 +0100

    begun work on client

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,18 +3,43 @@ import Home from "@components/Home.tsx";
 import {DevTools} from "jotai-devtools";
 import 'jotai-devtools/styles.css'
 import {Toaster} from "react-hot-toast"
-import {finalUrl} from "../core/baseUrl"
-import {useEffect} from "react";
+import {finalUrl, playerClient} from "../core/baseUrl"
+import {useEffect, useState} from "react";
+import type {Player} from "@core/generated-client.ts";
 
 
 function App() {
 
-    useEffect(()=> {
+    const [players, setPlayers] = useState<Player[]>([])
 
-    })
+    useEffect(()=> {
+        playerClient.getAllPlayers().then(r=> {
+            setPlayers(r);
+        })
+    }, [])
 
     return (
-        <>
+
+        <div>
+
+        <input placeholder="bingus"/>
+        <input placeholder="getmeoutofhere"/>
+        <button>CLIQUE MOI</button>
+
+
+        <hr/>
+
+
+            {
+                players.map(t => {
+                    return <div key={t.id}>
+
+                        {JSON.stringify(t)}
+                    </div>
+                })
+            }
+
+
             <h1>Look at me! Im a webpage!</h1>
 
             <div>
@@ -33,7 +58,7 @@ function App() {
                 position="top-center"
                 reverseOrder={false}
             />
-        </>
+        </div>
     )
 }
 
--- client/src/core/baseUrl.ts
+++ client/src/core/baseUrl.ts
@@ -1,7 +1,13 @@
+import {PlayerClient} from "@core/generated-client.ts";
+
 const isProduction = import.meta.env.PROD;
 
  const prod = "https://projectsolutionserver-quiet-rain-8134.fly.dev/";
  const dev = "http://localhost:5174";
- const todoClient = new TodoClient
+
 
 export const finalUrl = isProduction ? prod : dev;
+
+
+export const playerClient = new PlayerClient(finalUrl)
+
